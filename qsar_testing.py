a2c732f21075f2d00cf98972efda4cd856b6d539
Alejandro R. López <alejandrorlopezrodriguez@gmail.com>
2022-05-01 16:49:15 +0200

    Load and transform Docking data

--- qsar_testing.py
+++ qsar_testing.py
@@ -1,8 +1,11 @@
 # Cargar las librerias
+import sys
 import pickle
 from tensorflow import keras
 import pandas as pd
+import numpy as np
 from qsar_pckg import qsar_modelling as qm
+from qsar_pckg import chembl_screening as cs
 
 # Cargar los datos del script main
 
@@ -19,18 +22,18 @@ ann_model_max = keras.models.load_model('ann_model_max.h5')
 ann_model_mean = keras.models.load_model('ann_model_mean.h5')
 
 ## Importar el conjunto de test
-test_max_X = pd.read_csv('test_max_X.csv', header=1)
-test_max_Y = pd.read_csv('test_max_Y.csv', header=1)
-test_mean_X = pd.read_csv('test_mean_X.csv', header=1)
-test_mean_Y = pd.read_csv('test_mean_Y.csv', header=1)
+test_max_X = pd.read_csv('test_max_X.csv')
+test_max_Y = pd.read_csv('test_max_Y.csv')
+test_mean_X = pd.read_csv('test_mean_X.csv')
+test_mean_Y = pd.read_csv('test_mean_Y.csv')
 
 # Evaluar los modelos
 
 ## Crear listas de los modelos para iterarlos
-models_max = [rf_model_max, svm_model_max, nb_model_max, xgb_model_max, ann_model_max]
-models_mean = [rf_model_mean, svm_model_mean, nb_model_mean, xgb_model_mean, ann_model_mean]
+models_max = [rf_model_max, svm_model_max, nb_model_max, xgb_model_max]
+models_mean = [rf_model_mean, svm_model_mean, nb_model_mean, xgb_model_mean]
 
-## Definir un dataframe que almacenar· los resultados
+## Definir un dataframe que almacenar los resultados
 results_test = pd.DataFrame()
 
 ## Evaluar cada modelo en su conjunto de test
@@ -38,21 +41,34 @@ for i in range(len(models_max)):
     ## Predecir sobre los conjuntos de test con el modelo adecuado
     pred_max = models_max[i].predict(test_max_X)
     pred_mean = models_mean[i].predict(test_mean_X)
-    
-    ## Obtener las mÈtricas de calidad de las predicciones
+
+    ## Obtener las m√©tricas de calidad de las predicciones
     qm_max = qm.getQualityMetrics(test_max_Y, pred_max)
     qm_mean = qm.getQualityMetrics(test_mean_Y, pred_mean)
+
+    ## A√±adir los resultados al dataframe auxiliar
+    results_test = results_test.append(qm_max, ignore_index=True)
+    results_test = results_test.append(qm_mean, ignore_index=True)
     
-    ## AÒadir los resultados al dataframe auxiliar
-    results_test.append(qm_max, ignore_index=True)
-    results_test.append(qm_mean, ignore_index=True)
+## Evaluar de forma diferente ANN (produce valores cont√≠nuos)
+### Predecir sobre los conjuntos de test
+pred_max = np.around(ann_model_max.predict(test_max_X)).astype(int)
+pred_mean = np.around(ann_model_mean.predict(test_mean_X)).astype(int)
+
+### Obtener las m√©tricas de calidad de las predicciones
+qm_max = qm.getQualityMetrics(test_max_Y, pred_max)
+qm_mean = qm.getQualityMetrics(test_mean_Y, pred_mean)
+
+### A√±adir los resultados al dataframe auxiliar
+results_test = results_test.append(qm_max, ignore_index=True)
+results_test = results_test.append(qm_mean, ignore_index=True)
 
 # Almacenar los resultados
 
-## AÒadir los nombres de las columnas del df
+## A√±adir los nombres de las columnas del df
 results_test.columns = ['Balanced Accuracy', 'Sensitivity', 'Specificity', 'True Positives', 'False Positives', 'F-measure', 'AUC']
 
-## AÒadir columnas descriptivas
+## A√±adir columnas descriptivas
 results_test.insert(0, "Algorithm", [ a for a in ['Random Forest', 'Support Vector Machines', 'Naive Bayes', 'XGradientBoostTree', 'Artificial Neural Network']
                                       for i in range(2)], True)
 
@@ -60,3 +76,10 @@ results_test.insert(1, "Version", [ s for i in range(5) for s in ['Max', 'Mean']
 
 ## Exportar el dataframe a un .CSV
 results_test.to_csv('results_test.csv', index=False)
+
+# Cargar y transformar los datos del estudio por Docking
+dock_data = pd.read_excel(sys.argv[1])
+data_dnmt1_dock = cs.getSmiles(dock_data, 1)
+data_dnmt1_dock.to_csv('data_dnmt1_dock.csv', index=False)
+
+
